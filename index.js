296da06c4684d27866782764585e5cc961b64fc4
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-03-04 21:28:29 +0000

    removed debug logging

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ exports.Server = function bootstrap(s) {
     const port = spec.httpport || 8080
     const dirname = spec.dirname || '/public'
     
-    var server = createServer(dirname);
-    server.listen(port);
+    var server = createServer(dirname)
+    server.listen(port)
 
-    console.log("http server listening on %d", port);
+    console.log("http server listening on %d", port)
 
     const wss = new WebSocketServer({
         server: server
     })
     
-    console.log("ws server listening on %d", wss.options.port || 8080);
+    console.log("ws server listening on %d", wss.options.port || 8080)
     
-    return server
+    return wss
 }
 
 function createServer(dir) {
@@ -31,46 +31,45 @@ function createServer(dir) {
         "png": "image/png",
         "js": "text/javascript",
         "css": "text/css"
-    };
+    }
 
     return http.createServer(function(req, res) {
-        var uri = url.parse(req.url).pathname;
-        var unescapedUri = unescape(uri);
-        if (unescapedUri === '/') unescapedUri = 'index.html';
-        var filename = path.join(process.cwd() + dir, unescapedUri);
-        console.log('handling:' + filename);
-        var stats;
+        var uri = url.parse(req.url).pathname
+        var unescapedUri = unescape(uri)
+        if (unescapedUri === '/') unescapedUri = 'index.html'
+        var filename = path.join(process.cwd() + dir, unescapedUri)
+        var stats
 
         try {
-            stats = fs.lstatSync(filename); // throws if path doesn't exist
+            stats = fs.lstatSync(filename) // throws if path doesn't exist
         }
         catch (e) {
             res.writeHead(415, {
                 'Content-Type': 'text/plain'
-            });
-            res.write('404 Not Found\n');
-            res.end();
-            return;
+            })
+            res.write('404 Not Found\n')
+            res.end()
+            return
         }
 
 
         if (stats.isFile()) {
             // path exists, is a file
-            var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+            var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]]
             res.writeHead(200, {
                 'Content-Type': mimeType
-            });
+            })
 
-            var fileStream = fs.createReadStream(filename);
-            fileStream.pipe(res);
+            var fileStream = fs.createReadStream(filename)
+            fileStream.pipe(res)
         }
         else {
             res.writeHead(415, {
                 'Content-Type': 'text/plain'
-            });
-            res.write('404 Not Found\n');
-            res.end();
-            return;
+            })
+            res.write('404 Not Found\n')
+            res.end()
+            return
         }
 
     })
